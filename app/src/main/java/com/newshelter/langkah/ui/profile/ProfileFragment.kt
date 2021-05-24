5037055b567e3aa919fc97e458796060b271db6c
lazuardiap <lazuardiazhar99@gmail.com>
2021-05-24 21:44:51 +0700

    Profile Page Done, going to nail edit page

--- app/src/main/java/com/newshelter/langkah/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/newshelter/langkah/ui/profile/ProfileFragment.kt
@@ -1,13 +1,16 @@
 package com.newshelter.langkah.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import com.newshelter.langkah.LandingActivity
 import com.newshelter.langkah.R
 import com.newshelter.langkah.databinding.FragmentProfileBinding
+import com.newshelter.langkah.utils.DataDummy
 
 
 class ProfileFragment : Fragment() {
@@ -23,14 +26,65 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        if (activity != null){
-            viewModel = ProfileViewModel()
-            viewModel.text.observe(viewLifecycleOwner, Observer {
-                binding.textDashboard.text = it
-            })
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val dummyUsers = DataDummy.generateDummyUser()
+
+        val user = dummyUsers[2]
+
+        val fm = childFragmentManager
+        val mNoStatusFragment = NoStatusFragment()
+
+        val mOnProcessStatusFragment = OnProcessStatusFragment()
+
+        val mMadeStatusFragment = MadeStatusFragment()
+        val bundle = Bundle()
+
+        mMadeStatusFragment.arguments = bundle
+
+        bundle.putString(MadeStatusFragment.EXTRA_USER_ID, user.userId)
+
+        with(binding){
+            tvUserName.text = user.userFullName
+            tvUserEmail.text = user.userEmail
+            tvUserPhone.text = user.phoneNumber
+
+//            Status = 3 = Belum Buat Janji
+//            Status = 2 = Janji lagi di proses sama admin
+//            Status = 1 = Janji dibuat
+
+            btnSignOut.setOnClickListener {
+                val i = Intent(context, LandingActivity::class.java)
+                startActivity(i)
+            }
+
+            if (user.appointmentStatus == "3"){
+                tvNoStatus.text = context?.getString(R.string.no_appointment)
+                fm.beginTransaction()
+                    .add(R.id.frame_appointment_container, mNoStatusFragment, NoStatusFragment::class.java.simpleName)
+                    .commit()
+
+            } else if (user.appointmentStatus == "2"){
+                tvNoStatus.visibility = View.INVISIBLE
+                tvProcessStatus.visibility = View.VISIBLE
+                tvProcessStatus.text = context?.getString(R.string.appointment_on_process)
+
+                fm.beginTransaction()
+                    .add(R.id.frame_appointment_container, mOnProcessStatusFragment, OnProcessStatusFragment::class.java.simpleName)
+                    .commit()
+
+            } else {
+                tvNoStatus.visibility = View.INVISIBLE
+                tvMadeStatus.visibility = View.VISIBLE
+                tvMadeStatus.text = context?.getString(R.string.appointment_made)
+
+                fm.beginTransaction()
+                    .add(R.id.frame_appointment_container, mMadeStatusFragment, MadeStatusFragment::class.java.simpleName)
+                    .commit()
+            }
+
         }
     }
 
